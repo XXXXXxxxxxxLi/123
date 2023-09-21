163d3ed0311f3a79850d540a0620addd90a5bb74
李长法 <13265615+li-changfa@user.noreply.gitee.com>
2023-09-21 09:27:22 +0800

    对仓库进行拆分

--- latex_project/latex_app/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("select/", views.select_chapters, name="select_chapters"),
-]
--- 第一次做的项目/upphotoplus/config.py
+++
@@ -1,2 +0,0 @@
-DB_URI = "postgresql://postgres:ai_tongue@172.18.0.3:5432/postgres"
-IMAGE_PATH = "/home/user1/photo"
